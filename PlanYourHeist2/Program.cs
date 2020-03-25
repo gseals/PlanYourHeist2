a10b6fa03cdfce91f69b6b12e6e948be47568598
Gabriel Seals <sealsgj@gmail.com>
2020-03-25 18:48:54 -0500

    Add project files.

---
+++ PlanYourHeist2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace PlanYourHeist2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Plan your heist!");
+            Console.ReadLine();
+            Console.WriteLine("Enter your first tea member's name.");
+            var burglarName = Console.ReadLine();
+
+            Console.WriteLine($"{burglarName}. Good choice. Now, what is {burglarName}'s skill level? This number cannot be negative. Otherwise, this value will be randomly assigned.");
+            var skill = Console.ReadLine();
+            var numSkill = Int32.Parse(skill);
+
+            Console.WriteLine($"Next, what is {burglarName}'s courage factor? This number must be between 0.0 and 2.0. Otherwise, it will be randomly assigned.");
+            var courage = Console.ReadLine();
+            var numCourage = Convert.ToDouble(courage);
+
+            var wholeMember = new TeamMember(burglarName, numSkill, numCourage);
+
+            Console.WriteLine($"Your team member, {burglarName}, has a skill level of {numSkill} {wholeMember.SkillLevel} and a courage factor of {numCourage} {wholeMember.CourageFactor}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PlanYourHeist2/TeamMember.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanYourHeist2
+{
+    class TeamMember
+    {
+        private int _skillLevel;
+        private double _courageFactor;
+
+        public string Name { get; set; }
+
+        public int SkillLevel
+        {
+            get
+            {
+                return _skillLevel;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _skillLevel = value;
+                }
+                else
+                {
+                    Random rnd = new Random();
+                    _skillLevel = rnd.Next(1000);
+                }
+            }
+        }
+
+        public double CourageFactor
+        {
+            get
+            {
+                return _courageFactor;
+            }
+            set
+            {
+                if (value > 0.0 && value < 2.0)
+                {
+                    _courageFactor = value;
+                }
+                else
+                {
+                    Random rnd2 = new Random();
+                    _courageFactor = rnd2.NextDouble() * 2;
+                }
+            }
+
+        }
+
+        public TeamMember(string name, int skillLevel, double courageFactor)
+        {
+            Name = name;
+            SkillLevel = skillLevel;
+            CourageFactor = courageFactor;
+        }
+
+
+    }
+}
